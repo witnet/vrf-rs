bb91520a410e2cb3221e39e630d10ae6307aab8b
Mario Cao <mario@stampery.co>
2019-04-24 13:00:12 +0200

    feat(dummy): add dummy implementation
    Co-authored-by: Gorka Irazoqui <gorka.irazoki@gmail.com>

---
+++ src/dummy.rs
@@ -0,0 +1,53 @@
+use crate::VRF;
+use failure::Error;
+
+struct DummyVRF;
+
+/// The size (in bytes) of a secret key
+pub const SECRET_KEY_SIZE: usize = 32;
+
+/// The size (in bytes) of a serialized public key.
+pub const PUBLIC_KEY_SIZE: usize = 33;
+
+/// The type of the secret key
+pub type SecretKey<'a> = &'a [u8; SECRET_KEY_SIZE];
+
+/// The type of the public key
+pub type PublicKey<'a> = &'a [u8; PUBLIC_KEY_SIZE];
+
+impl<'a> VRF<PublicKey<'a>, SecretKey<'a>> for DummyVRF {
+    type Error = Error;
+
+    // Generate proof from key pair and message
+    fn prove(_x: SecretKey, _alpha: &[u8]) -> Result<Vec<u8>, Self::Error> {
+        Ok(vec![])
+    }
+
+    // Verify proof given public key, proof and message
+    fn verify(_y: PublicKey, _pi: &[u8], _alpha: &[u8]) -> Result<bool, Self::Error> {
+        Ok(false)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_prove() {
+        let x = [0; 32];
+        let alpha = [0, 0, 0];
+
+        let proof = DummyVRF::prove(&x, &alpha);
+        assert_eq!(proof.unwrap(), vec![]);
+    }
+
+    #[test]
+    fn test_verify() {
+        let y = [0; 33];
+        let pi = [0];
+        let alpha = [0, 0, 0];
+
+        assert_eq!(DummyVRF::verify(&y, &pi, &alpha).unwrap(), false);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod dummy;
+mod p256v1;
+
+/// A trait for a Verifiable Random Functions (VRF)
+/// implementations.
+pub trait VRF<PublicKey, SecretKey> {
+    type Error;
+
+    /// Generate proof from key pair and message
+    fn prove(x: SecretKey, alpha: &[u8]) -> Result<Vec<u8>, Self::Error>;
+
+    /// Verify proof given public key, proof and message
+    fn verify(y: PublicKey, pi: &[u8], alpha: &[u8]) -> Result<bool, Self::Error>;
 }
