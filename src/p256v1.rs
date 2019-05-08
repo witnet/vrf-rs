14378104c3fde38708986b5762f7e40313134fd5
Mario Cao <mario@stampery.co>
2019-05-08 11:22:55 +0200

    feat(ecvrf): add VRF prove function for EC curves
     Co-authored-by: Gorka Irazoqui <gorka.irazoki@gmail.com>

--- src/p256v1.rs
+++ src/p256v1.rs
@@ -1,6 +1,5 @@
-use std::os::raw::c_ulong;
-
 use failure::Fail;
+use hmac_sha256::HMAC;
 use openssl::{
     bn::{BigNum, BigNumContext},
     ec::{EcGroup, EcPoint, PointConversionForm},
@@ -8,6 +7,7 @@ use openssl::{
     hash::{hash, MessageDigest},
     nid::Nid,
 };
+use std::os::raw::c_ulong;
 
 use crate::VRF;
 
@@ -18,9 +18,6 @@ const SECRET_KEY_SIZE: usize = 32;
 const PUBLIC_KEY_SIZE: usize = 33;
 
 /// Cipher suite for EC p256v1 curve
-const P256V1_N: usize = 16;
-
-/// Cipher suite for EC p256v1 curve
 const P256V1_CIPHER_SUITE: u8 = 0x01;
 
 /// Prefix used for the hash to point function
@@ -29,11 +26,10 @@ const HASH_TO_POINT_PREFIX: u8 = 0x01;
 /// Prefix used for the hash points function
 const HASH_POINTS_PREFIX: u8 = 0x02;
 
-/// The type of the secret key
-type SecretKey<'a> = &'a [u8; SECRET_KEY_SIZE];
-
-/// The type of the public key
-type PublicKey<'a> = &'a [u8; PUBLIC_KEY_SIZE];
+enum Curve {
+    NISTP256,
+    SECT163K1,
+}
 
 /// Error that can be raised when proving/verifying VRFs
 #[derive(Debug, Fail)]
@@ -42,13 +38,15 @@ pub enum Error {
     CodedError { code: c_ulong },
     #[fail(display = "Hash to point function could not find a valid point")]
     HashToPointError,
+    #[fail(display = "Float division error while computing remainder")]
+    IntegerDivisionError,
     #[fail(display = "Unknown error")]
     Unknown,
 }
 
 impl From<ErrorStack> for Error {
     fn from(error: ErrorStack) -> Self {
-        match error.errors().get(0).map(|e| e.code()) {
+        match error.errors().get(0).map(openssl::error::Error::code) {
             Some(code) => Error::CodedError { code },
             _ => Error::Unknown {},
         }
@@ -57,38 +55,99 @@ impl From<ErrorStack> for Error {
 
 /// Elliptic Curve context
 struct ECContext {
+    curve: Curve,
     group: EcGroup,
     bn_ctx: BigNumContext,
+    order: BigNum,
     hasher: MessageDigest,
+    n: usize,
+    qlen: usize,
 }
 
 /// A Elliptic Curve VRF using the curve p256v1
-struct P256v1;
+pub struct ECVRF;
 
-impl<'a> VRF<PublicKey<'a>, SecretKey<'a>> for P256v1 {
+impl<'a> VRF<&'a [u8], &'a [u8]> for ECVRF {
     type Error = Error;
 
     // Generate proof from key pair and message
-    fn prove(_x: SecretKey, _alpha: &[u8]) -> Result<Vec<u8>, Error> {
-        Ok(vec![])
-    }
+    fn prove(x: &[u8], alpha: &[u8]) -> Result<Vec<u8>, Error> {
+        let mut ctx = create_ec_context(Curve::NISTP256)?;
 
+        // Step 1: derive public key from secret key
+        // Y = x * B
+        //TODO: validate secret key length?
+        let secret_key = BigNum::from_slice(x)?;
+        let public_key_point = derive_public_key(&secret_key, &ctx)?;
+
+        // Step 2: Hash to curve
+        let h_point = hash_to_try_and_increment(&public_key_point, alpha, &mut ctx)?;
+
+        // Step 3: point to string
+        let h_string =
+            h_point.to_bytes(&ctx.group, PointConversionForm::COMPRESSED, &mut ctx.bn_ctx)?;
+
+        // Step 4: Gamma = x * H
+        let mut gamma_point = EcPoint::new(&ctx.group.as_ref())?;
+        gamma_point.mul(&ctx.group.as_ref(), &h_point, &secret_key, &mut ctx.bn_ctx);
+
+        // Step 5: nonce
+        let k = nonce_generation_rfc6979(&secret_key, &h_string, &mut ctx)?;
+
+        // Step 6: c = hash points(...)
+        let mut u_point = EcPoint::new(&ctx.group.as_ref())?;
+        let mut v_point = EcPoint::new(&ctx.group.as_ref())?;
+        u_point.mul_generator(&ctx.group.as_ref(), &k, &mut ctx.bn_ctx);
+        v_point.mul(&ctx.group.as_ref(), &h_point, &k, &mut ctx.bn_ctx);
+        let mut c = hash_points(&[&h_point, &gamma_point, &u_point, &v_point], &mut ctx)?;
+
+        // Step 7: s = (k + c*x) mod q
+        let s = &(&k + &(&c * &secret_key)) % &ctx.order;
+
+        // Step 8: encode (gamma, c, s)
+        let gamma_string =
+            gamma_point.to_bytes(&ctx.group, PointConversionForm::COMPRESSED, &mut ctx.bn_ctx)?;
+        let c_string = append_leading_zeros(&c.to_vec(), ctx.n)?;
+        let s_string = append_leading_zeros(&s.to_vec(), ctx.qlen)?;
+        let proof = [&gamma_string[..], &c_string, &s_string].concat();
+
+        Ok(proof)
+    }
     // Verify proof given public key, proof and message
-    fn verify(_y: PublicKey, _pi: &[u8], _alpha: &[u8]) -> Result<bool, Error> {
+    fn verify(_y: &[u8], _pi: &[u8], _alpha: &[u8]) -> Result<bool, Error> {
         Ok(false)
     }
 }
 
 /// Function to create a Elliptic Curve context using the curve prime256v1
-fn create_ec_context() -> Result<ECContext, Error> {
-    let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1)?;
-    let bn_ctx = BigNumContext::new()?;
+fn create_ec_context(curve: Curve) -> Result<ECContext, Error> {
+    let group = match curve {
+        Curve::NISTP256 => EcGroup::from_curve_name(Nid::X9_62_PRIME256V1)?,
+        Curve::SECT163K1 => EcGroup::from_curve_name(Nid::SECT163K1)?,
+    };
+    let mut bn_ctx = BigNumContext::new()?;
     let hasher = MessageDigest::sha256();
+    let order = BigNum::new().map(|mut ord| {
+        group.order(&mut ord, &mut bn_ctx);
+        ord
+    })?;
+
+    let mut a = BigNum::new()?;
+    let mut b = BigNum::new()?;
+    let mut p = BigNum::new()?;
+    group.components_gfp(&mut a, &mut b, &mut p, &mut bn_ctx)?;
+
+    let n = ((p.num_bits() + (p.num_bits() % 2)) / 2) as usize;
+    let qlen = order.num_bits() as usize;
 
     Ok(ECContext {
+        curve,
         group,
         bn_ctx,
+        order,
         hasher,
+        n,
+        qlen,
     })
 }
 
@@ -105,17 +164,13 @@ fn hash_to_try_and_increment(
     alpha: &[u8],
     mut ctx: &mut ECContext,
 ) -> Result<EcPoint, Error> {
-    let c = 0..255;
+    let mut c = 0..255;
     let pk_bytes =
         public_key.to_bytes(&ctx.group, PointConversionForm::COMPRESSED, &mut ctx.bn_ctx)?;
-    let mut v = vec![];
-    let cipher = [P256V1_CIPHER_SUITE, HASH_TO_POINT_PREFIX];
-    v.extend(&cipher);
-    v.extend(pk_bytes.clone());
-    v.extend(alpha.clone());
-    v.push(0);
+    let cipher = [0x01, 0x01];
+    let mut v = [&cipher[..], &pk_bytes[..], &alpha[..], &[0x00]].concat();
     let position = v.len() - 1;
-    let point = c.into_iter().find_map(|ctr| {
+    let point = c.find_map(|ctr| {
         v[position] = ctr;
         let attempted_hash = hash(ctx.hasher, &v);
         match attempted_hash {
@@ -134,8 +189,102 @@ fn arbitrary_string_to_point(data: &[u8], ctx: &mut ECContext) -> Result<EcPoint
     Ok(point)
 }
 
+fn append_leading_zeros(data: &[u8], length: usize) -> Result<Vec<u8>, Error> {
+    if data.len() * 8 > length {
+        return Ok(data.to_vec());
+    }
+
+    let leading_zeros = if length % 8 > 0 {
+        vec![0; length / 8 + 1 - data.len()]
+    } else {
+        vec![0; length / 8 - data.len()]
+    };
+    let padded_bytes = [&leading_zeros[..], &data].concat();
+
+    Ok(padded_bytes)
+}
+
+fn nonce_generation_rfc6979(
+    secret_key: &BigNum,
+    data: &[u8],
+    ctx: &mut ECContext,
+) -> Result<BigNum, Error> {
+    // Bits to octets from data - bits2octets(h1)
+    // Length of this value should be dependent on qlen (i.e. SECP256k1 is 32)
+    let data_trunc = bits2octets(data, ctx)?;
+    let padded_data_trunc = append_leading_zeros(&data_trunc, ctx.qlen)?;
+
+    // Bytes to octets from secret key - int2octects(x)
+    // Left padding is required for inserting leading zeros
+    let padded_secret_key_bytes: Vec<u8> = append_leading_zeros(&secret_key.to_vec(), ctx.qlen)?;
+
+    // Init V & K
+    // K = HMAC_K(V || 0x00 || int2octects(secret_key) || bits2octects(data))
+    let mut v = [0x01; 32];
+    let mut k = [0x00; 32];
+
+    for prefix in 0..2 as u8 {
+        k = HMAC::mac(
+            [
+                &v[..],
+                &[prefix],
+                &padded_secret_key_bytes[..],
+                &padded_data_trunc[..],
+            ]
+                .concat()
+                .as_slice(),
+            &k,
+        );
+        v = HMAC::mac(&v, &k);
+    }
+
+    loop {
+        v = HMAC::mac(&v, &k);
+        let ret_bn = bits2int(&v, ctx.qlen)?;
+
+        if ret_bn > BigNum::from_u32(0)? && ret_bn < ctx.order {
+            return Ok(ret_bn);
+        }
+        k = HMAC::mac([&v[..], &[0x00]].concat().as_slice(), &k);
+        v = HMAC::mac(&v, &k);
+    }
+}
+
+fn bits2octets(data: &[u8], ctx: &mut ECContext) -> Result<Vec<u8>, Error> {
+    //FIXME: TO DECIDE WHETHER FOLLOW DIFFERENT TEST VECTORS (qlen for both cases)
+    let z1 = match ctx.curve {
+        Curve::NISTP256 => bits2int(data, data.len() * 8)?,
+        Curve::SECT163K1 => bits2int(data, ctx.qlen)?,
+    };
+    let result = BigNum::new().and_then(|mut res| {
+        res.nnmod(&z1, &ctx.order, &mut ctx.bn_ctx)?;
+        Ok(res.to_vec())
+    })?;
+
+    Ok(result)
+}
+
+/// Transforms slice into Bignum and right-shifts it by len(data)-qlen bits.
+fn bits2int(data: &[u8], qlen: usize) -> Result<BigNum, Error> {
+    let data_len_bits = data.len() * 8;
+    let result = BigNum::from_slice(data).and_then(|data_bn| {
+        if data_len_bits > qlen {
+            let mut truncated = BigNum::new()?;
+            truncated.rshift(&data_bn, (data_len_bits - qlen) as i32)?;
+
+            Ok(truncated)
+        } else {
+            Ok(data_bn)
+        }
+    })?;
+    let _data2 = data.to_vec();
+    let _data_vec = result.to_vec();
+
+    Ok(result)
+}
+
 /// Function to calculate the hash of multiple EC Points
-fn hash_points(points: &[EcPoint], ctx: &mut ECContext) -> Result<Vec<u8>, Error> {
+fn hash_points(points: &[&EcPoint], ctx: &mut ECContext) -> Result<BigNum, Error> {
     let point_bytes: Result<Vec<u8>, Error> = points.iter().try_fold(
         vec![P256V1_CIPHER_SUITE, HASH_POINTS_PREFIX],
         |mut acc, point| {
@@ -148,9 +297,10 @@ fn hash_points(points: &[EcPoint], ctx: &mut ECContext) -> Result<Vec<u8>, Error
     );
     let to_be_hashed = point_bytes?;
     let mut hash = hash(ctx.hasher, &to_be_hashed).map(|hash| hash.to_vec())?;
-    hash.truncate(P256V1_N);
+    hash.truncate(ctx.n / 8);
+    let result = BigNum::from_slice(hash.as_slice())?;
 
-    Ok(hash)
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -158,12 +308,18 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_prove() {
-        let x = [0; 32];
-        let alpha = [0, 0, 0];
+    fn test_prove_P256_SHA256() {
+        // Secret Key (labelled as x)
+        let x = hex::decode("c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721")
+            .unwrap();
 
-        let proof = P256v1::prove(&x, &alpha);
-        assert_eq!(proof.unwrap(), vec![]);
+        // Data to be hashed: ASCII "sample
+        let alpha = hex::decode("73616d706c65").unwrap();
+
+        let expected_pi = hex::decode("029bdca4cc39e57d97e2f42f88bcf0ecb1120fb67eb408a856050dbfbcbf57c524193b7a850195ef3d5329018a8683114cb446c33fe16ebcc0bc775b043b5860dcb2e553d91268281688438df9394103ab").unwrap();
+
+        let pi = ECVRF::prove(&x, &alpha).unwrap();
+        assert_eq!(pi, expected_pi);
     }
 
     #[test]
@@ -172,13 +328,13 @@ mod test {
         let pi = [0];
         let alpha = [0, 0, 0];
 
-        assert_eq!(P256v1::verify(&y, &pi, &alpha).unwrap(), false);
+        assert_eq!(ECVRF::verify(&y, &pi, &alpha).unwrap(), false);
     }
 
     #[test]
     fn test_derive_public_key() {
         let k = [0x01];
-        let mut ctx = create_ec_context().unwrap();
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
 
         let secret_key = BigNum::from_slice(&k).unwrap();
         let expected = [
@@ -193,9 +349,11 @@ mod test {
             .unwrap());
     }
 
+    /// Hash to try and increment (TAI) test
+    /// Test vector extracted from VRF RFC draft (section A.1)
     #[test]
     fn test_hash_to_try_and_increment() {
-        let mut ctx = create_ec_context().unwrap();
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
         let public_key_hex =
             hex::decode("0360fed4ba255a9d31c961eb74c6356d68c049b8923b61fa6ce669622e60f29fb6")
                 .unwrap();
@@ -205,6 +363,7 @@ mod test {
                 .unwrap();
         let expected_hash =
             EcPoint::from_bytes(&ctx.group, &expected_hash_hex, &mut ctx.bn_ctx).unwrap();
+        // Data to be hashed: ASCII "sample
         let data = hex::decode("73616d706c65").unwrap();
         let derived_hash = hash_to_try_and_increment(&public_key, &data, &mut ctx).unwrap();
         assert!(derived_hash
@@ -214,7 +373,9 @@ mod test {
 
     #[test]
     fn test_hash_to_try_and_increment_2() {
-        let mut ctx = create_ec_context().unwrap();
+        // Example of using a different hashing function
+        let suite: u8 = 1;
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
         let public_key_hex =
             hex::decode("03596375e6ce57e0f20294fc46bdfcfd19a39f8161b58695b3ec5b3d16427c274d")
                 .unwrap();
@@ -231,9 +392,141 @@ mod test {
             .unwrap());
     }
 
+    /// Nonce generation test using the curve K-163
+    /// Test vector extracted from RFC6979 (section A.1)
+    #[test]
+    fn test_nonce_generation_RFC6979_SECT163K1() {
+        let mut ctx = create_ec_context(Curve::SECT163K1).unwrap();
+        let mut ord = BigNum::new().unwrap();
+        ctx.group.order(&mut ord, &mut ctx.bn_ctx).unwrap();
+
+        // Expected result/nonce (labelled as K or T)
+        // This is the va;ue of T
+        let expected_nonce = hex::decode("023AF4074C90A02B3FE61D286D5C87F425E6BDD81B").unwrap();
+
+        // Secret Key (labelled as x)
+        let sk = hex::decode("009A4D6792295A7F730FC3F2B49CBC0F62E862272F").unwrap();
+        let sk_bn = BigNum::from_slice(&sk).unwrap();
+
+        // Hashed input message (labelled as h1)
+        let data = hex::decode("AF2BDBE1AA9B6EC1E2ADE1D694F41FC71A831D0268E9891562113D8A62ADD1BF")
+            .unwrap();
+        let data_bn = BigNum::from_slice(&data).unwrap();
+
+        // Nonce generation
+        let derived_nonce = nonce_generation_rfc6979(&sk_bn, &data, &mut ctx).unwrap();
+
+        assert_eq!(derived_nonce.to_vec(), expected_nonce);
+    }
+
+    #[test]
+    fn test_nonce_generation_RFC6979_NISTP256() {
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
+        let mut ord = BigNum::new().unwrap();
+        let mut a = BigNum::new().unwrap();
+        let mut b = BigNum::new().unwrap();
+        let mut p = BigNum::new().unwrap();
+        ctx.group
+            .components_gfp(&mut a, &mut b, &mut p, &mut ctx.bn_ctx)
+            .unwrap();
+
+        // Expected result/nonce (labelled as K or T)
+        // This is the va;ue of T
+        let expected_nonce =
+            hex::decode("A6E3C57DD01ABE90086538398355DD4C3B17AA873382B0F24D6129493D8AAD60")
+                .unwrap();
+
+        // Secret Key (labelled as x)
+        let sk = hex::decode("c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721")
+            .unwrap();
+        let sk_bn = BigNum::from_slice(&sk).unwrap();
+
+        // Hashed input message (labelled as h1)
+        //FIXME: TO CHECK if 0x02 is correct
+        let data = hex::decode("AF2BDBE1AA9B6EC1E2ADE1D694F41FC71A831D0268E9891562113D8A62ADD1BF")
+            .unwrap();
+        let data_bn = BigNum::from_slice(&data).unwrap();
+
+        // Nonce generation
+        let derived_nonce = nonce_generation_rfc6979(&sk_bn, &data, &mut ctx).unwrap();
+        assert_eq!(derived_nonce.to_vec(), expected_nonce);
+    }
+
+    #[test]
+    fn test_nonce_generation_RFC6979_NISTP256_2() {
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
+        let mut ord = BigNum::new().unwrap();
+        ctx.group.order(&mut ord, &mut ctx.bn_ctx).unwrap();
+        // Expected result/nonce (labelled as K or T)
+        // This is the value of T
+        let expected_nonce =
+            hex::decode("D16B6AE827F17175E040871A1C7EC3500192C4C92677336EC2537ACAEE0008E0")
+                .unwrap();
+
+        // Secret Key (labelled as x)
+        let sk = hex::decode("c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721")
+            .unwrap();
+        let sk_bn = BigNum::from_slice(&sk).unwrap();
+
+        // Hashed input message (labelled as h1)
+        //FIXME: TO CHECK if 0x02 is correct
+        let data = hex::decode("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
+            .unwrap();
+        let data_bn = BigNum::from_slice(&data).unwrap();
+
+        // Nonce generation
+        let derived_nonce = nonce_generation_rfc6979(&sk_bn, &data, &mut ctx).unwrap();
+        assert_eq!(derived_nonce.to_vec(), expected_nonce);
+    }
+
+    #[test]
+    fn test_nonce_generation_RFC6979_NISTP256_3() {
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
+        let mut ord = BigNum::new().unwrap();
+        ctx.group.order(&mut ord, &mut ctx.bn_ctx).unwrap();
+        // Expected result/nonce (labelled as K or T)
+        // This is the va;ue of T
+        let expected_nonce =
+            hex::decode("c1aba586552242e6b324ab4b7b26f86239226f3cfa85b1c3b675cc061cf147dc")
+                .unwrap();
+
+        // Secret Key (labelled as x)
+        let sk = hex::decode("c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721")
+            .unwrap();
+        let sk_bn = BigNum::from_slice(&sk).unwrap();
+
+        // Hashed input message (labelled as h1)
+        //FIXME: TO CHECK if 0x02 is correct
+        let data =
+            hex::decode("02e2e1ab1b9f5a8a68fa4aad597e7493095648d3473b213bba120fe42d1a595f3e")
+                .unwrap();
+        let data_bn = BigNum::from_slice(&data).unwrap();
+
+        // Nonce generation
+        let derived_nonce = nonce_generation_rfc6979(&sk_bn, &data, &mut ctx).unwrap();
+
+        assert_eq!(derived_nonce.to_vec(), expected_nonce);
+    }
+
+    #[test]
+    fn test_bits2int() {
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
+        let data1 = vec![0x01; 32];
+        let data1_bn = BigNum::from_slice(&data1).unwrap();
+        let result1 = bits2int(&data1, 256).unwrap();
+        assert_eq!(data1_bn, result1);
+
+        let data2 = vec![0x01; 33];
+        let data2_bn = BigNum::from_slice(&data2).unwrap();
+        let result2 = bits2int(&data2, 256).unwrap();
+        let mut truncated = BigNum::new().unwrap();
+        truncated.rshift(&data2_bn, 8);
+        assert_eq!(truncated.to_vec(), result2.to_vec());
+    }
+
     #[test]
     fn test_hash_points() {
-        let mut ctx = create_ec_context().unwrap();
+        let mut ctx = create_ec_context(Curve::NISTP256).unwrap();
 
         let hash_hex =
             hex::decode("02e2e1ab1b9f5a8a68fa4aad597e7493095648d3473b213bba120fe42d1a595f3e")
@@ -261,8 +554,8 @@ mod test {
         let v_point = EcPoint::from_bytes(&ctx.group, &v_hex, &mut ctx.bn_ctx).unwrap();
 
         let computed_c =
-            hash_points(&[hash_point, gamma_point, u_point, v_point], &mut ctx).unwrap();
+            hash_points(&[&hash_point, &gamma_point, &u_point, &v_point], &mut ctx).unwrap();
 
-        assert_eq!(computed_c, c_hex);
+        assert_eq!(computed_c.to_vec(), c_hex);
     }
 }
