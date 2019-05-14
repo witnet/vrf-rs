4762f1706ba032fc2dd192f6f01566f29daed9f2
Mario Cao <mario@stampery.co>
2019-05-14 17:59:19 +0200

    docs(lib): add VRF crate documentation
    Co-authored-by: Gorka Irazoqui <gorka.irazoki@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! # Verifiable Random Function (VRF)
+//!
+//! This crate defines the generic contract that must be followed by VRF implementations ([`VRF`](trait.VRF.html) trait).
+//!
+//! ## Elliptic Curve VRF
+//!
+//! The [`openssl`](openssl/index.html) module provides an implementation of Elliptic Curve VRF ([`ECVRF`](openssl/struct.ECVRF.html)).
+//!
+//! It follows the algorithms described in:
+//!
+//! * [VRF-draft-04](https://tools.ietf.org/pdf/draft-irtf-cfrg-vrf-04)
+//! * [RFC6979](https://tools.ietf.org/html/rfc6979)
+//!
+//! Currently the supported cipher suites are:
+//!
+//! * `P256_SHA256_TAI`: the aforementioned algorithms with `SHA256` and the `NIST P-256` curve.
+//! * `K163_SHA256_TAI`: the aforementioned algorithms with `SHA256` and the `NIST K-163` curve.
+//! * `SECP256K1_SHA256_TAI`: the aforementioned algorithms with `SHA256` and the `secp256k1` curve.
 pub mod dummy;
 pub mod openssl;
 
